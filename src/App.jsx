d3b66f52def57aacf8704ed9ff3018a88303b3af
mattismyname3011 <mattismyname3011@gmail.com>
2023-07-24 14:58:22 +0800

    404 Not Found

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 
 import Home from "./components/pages/home";
 import Docs from "./components/pages/docs";
 import Gallery from "./components/pages/gallery";
+import Error from "./components/pages/error";
 
 const App = () => {
   return (
@@ -14,6 +20,9 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/docs" element={<Docs />} />
         <Route path="/gallery" element={<Gallery />} />
+        <Route path="/error" element={<Error />} />
+        {/* <Route path="*" element={<Navigate to={<Error />} />} /> */}
+        <Route path="/*" element={<Navigate to="/error" />} />
       </Routes>
     </Router>
   );
